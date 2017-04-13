440710a592674d5bd5a44afe0143ac4b62d2f6a9
Kylin <kylin@192.168.1.3>
2017-04-13 23:54:45 +0800

    修改策略-启动

--- selector/plugin/qd.py
+++ selector/plugin/qd.py
@@ -8,14 +8,23 @@ from selector.plugin.zf import zf
 def qd(quote):
     nday = config.QD_NDAY
     df_quote = quote[len(quote)-nday:]['close'] # Series
-    close_min = df_quote.min()
     index_min = df_quote.argmin()
-    close_max = df_quote[index_min:].max()
+    close_min = df_quote[index_min]
+
+    index_min_max = df_quote[index_min:].argmax()
+    close_min_max = df_quote[index_min_max]
+
+    index_min_max_min = df_quote[index_min_max:].argmin()
+    close_min_max_min = df_quote[index_min_max_min]
+
     close_max_all = df_quote.max()
     close = df_quote[-1]
-    percent = (close_max - close_min)/close_min*100
+    percent_max = (close_min_max - close_min)/close_min*100
+    percent_ht_max = (close_min_max - close_min_max_min)/close_min_max*100
+    percent = (close - close_min)/close_min*100
+    #print(percent_max, percent_ht_max)
     # df_quote.mean()
-    if percent < config.QD_PERCENT_MIN or percent > config.QD_PERCENT_MAX or close_max < close_max_all:
+    if percent_max < config.QD_PERCENT_MIN or percent_max > config.QD_PERCENT_MAX or percent_ht_max > config.QD_PERCENT_HT_MAX or close_min_max < close_max_all:
         return False
 
     #print(quote['code'][-1], close_min, close_max, percent)
--- selector/plugin/zf.py
+++ selector/plugin/zf.py
@@ -8,7 +8,9 @@ def zf(quote):
     nday = config.ZF_NDAY
     df_quote = quote[len(quote)-nday:]['zf'] # Series
     # df_quote.mean()
-    if df_quote.min() > config.ZF_PERCENT_MIN and df_quote.mean() < config.ZF_PERCENT_AVG_MAX:
+    _p = len(df_quote[df_quote > config.ZF_PERCENT_MIN])/config.ZF_NDAY*100
+    #if df_quote.min() > config.ZF_PERCENT_MIN and df_quote.mean() < config.ZF_PERCENT_AVG_MAX:
+    if _p >= config.ZF_PERCENT_GT_MIN and df_quote.mean() < config.ZF_PERCENT_AVG_MAX:
         return True
 
     return False
